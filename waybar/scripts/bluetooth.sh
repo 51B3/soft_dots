02154f293c78cd18cff496a00ad9d2e271442bd9
Anton <85944021+51B3@users.noreply.github.com>
2026-01-25 08:02:00 +0700

    Commit without message

--- waybar/scripts/bluetooth.sh
+++ waybar/scripts/bluetooth.sh
@@ -5,8 +5,8 @@ STATUS=$(bluetoothctl show | grep "Powered: yes" | wc -l)
 if [ $STATUS -eq 0 ]; then
     echo "disabled"
 else
-    CONNECTED=$(bluetoothctl info | grep "Connected: yes" | wc -l)
-    if [ $CONNECTED -eq 0 ]; then
+    IS_CONNECTED=$(bluetoothctl info | grep "Connected: yes" | wc -l)
+    if [ $IS_CONNECTED -eq 0 ]; then
         echo "disconnected"
     else
         echo "connected"
